2f8db7a5787e142fc59dfa042ac3bd3fd4bcf6b1
PanchoFrontEnd <francisco.2a.insuco1dp@gmail.com>
2023-10-30 21:13:00 -0300

    se suben cambios

---
+++ scripts/storage.js
@@ -0,0 +1,23 @@
+
+// localstorage
+
+
+const usuarioEnLS = JSON.parse(localStorage.getItem("usuario"));
+
+console.log(usuarioEnLS);
+
+/** ARRAY DE OBJETOS **/
+const productos = [
+    { nombre: "Zapatillas Nike", precio: 80 },
+    { nombre: "Audifonos Beats", precio: 20 },
+    { nombre: "Reloj Casio", precio: 50 },
+    { nombre: "Smartwatch Apple", precio: 90 },
+    { nombre: "Perfume Coco Channel", precio: 50 },
+    
+
+];
+
+localStorage.setItem("productos", JSON.stringify(productos));
+
+const productosEnLS = JSON.parse(localStorage.getItem("productos"));
+console.log(productosEnLS);
