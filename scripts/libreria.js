294bb358a599c5c2ca61bcb214321a802908f2b0
PanchoFrontEnd <francisco.2a.insuco1dp@gmail.com>
2023-11-06 17:31:09 -0300

    se agregan mejoras

--- scripts/libreria.js
+++ scripts/libreria.js
@@ -1,16 +1,75 @@
 
-const btnAddcart = document.querySelector("#btnAddcart");
+const sweetAlert = document.querySelector("#sweetAlert")
 
-btnAddcart.addEventListener("click", () => {
+sweetAlert.addEventListener("click", () => {
 
-    Swal.fire({
-        title: 'Perfecto!',
-        text: 'Tu produco se añadio al carrito',
-        icon: 'success',
-        confirmButtonText: 'Continuar'
-      });
+  Swal.fire({
+    title: 'Perfecto!',
+    text: 'Tu produco se añadio al carrito',
+    icon: 'success',
+    confirmButtonText: 'Continuar'
+  });
 
 });
+
+const buttonBeats = document.querySelector("#buttoBeats")
+
+buttonBeats.addEventListener("click", () => {
+  
+  Swal.fire({
+    title: 'Perfecto!',
+    text: 'Tu produco se añadio al carrito',
+    icon: 'success',
+    confirmButtonText: 'Continuar'
+  });
+
+
+})
+
+const buttonCasio = document.querySelector("#buttonCasio")
+
+buttonCasio.addEventListener("click", () => {
+
+  Swal.fire({
+    title: 'Perfecto!',
+    text: 'Tu produco se añadio al carrito',
+    icon: 'success',
+    confirmButtonText: 'Continuar'
+  });
+
+});
+
+const buttonApple = document.querySelector("#buttonApple")
+
+buttonApple.addEventListener("click", () => {
+
+  Swal.fire({
+    title: 'Perfecto!',
+    text: 'Tu produco se añadio al carrito',
+    icon: 'success',
+    confirmButtonText: 'Continuar'
+  });
+
+});
+
+const buttonChanel = document.querySelector("#buttonChanel")
+
+buttonChanel.addEventListener("click", () => {
+
+  Swal.fire({
+    title: 'Perfecto!',
+    text: 'Tu produco se añadio al carrito',
+    icon: 'success',
+    confirmButtonText: 'Continuar'
+  });
+
+});
+
+
+
+
+
+
 
 
 
